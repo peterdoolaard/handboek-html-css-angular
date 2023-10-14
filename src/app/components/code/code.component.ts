6be4538d9d36bec174872cebb056427c5f77523f
peterdoolaard <peter.doolaard@xs4all.nl>
2023-10-15 00:10:08 +0200

    Implementing live code editing with mutationobserver

--- src/app/components/code/code.component.ts
+++ src/app/components/code/code.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { combineLatest, Observable, of, switchMap, tap } from "rxjs";
 
 import { AppSharedService } from "../../services/app-shared.service";
@@ -12,13 +12,32 @@ import { CodeExample } from "../../models";
   styleUrls: ['./code.component.scss'],
 })
 export class CodeComponent implements OnInit {
+  @ViewChild('codeWrapper') divCodeWrapper!: ElementRef;
+  @ViewChild('btnReset') btnReset!: ElementRef;
+  @ViewChild('exampleView') divExampleView!: ElementRef;
+  @ViewChild('exampleHtml') exampleHtml!: ElementRef;
+  @ViewChild('exampleCss') exampleCss!: ElementRef;
+  @ViewChild('exampleStyle') exampleStyle!: ElementRef;
+
   codeExamples$: Observable<CodeExample[]> | undefined;
+  observeCodeWrapper = new MutationObserver(mutations => this.updateView(mutations))
+  config = {
+    attributes: false,
+    subtree: true,
+    childList: false,
+    characterData: true,
+    characterDataOldValue: false
+  };
 
   constructor(
     private sharedService: AppSharedService,
     private highlightService: HighlightService,
-  ) {}
+  ) {
+  }
 
+  // Bewerkingen op de codevoorbeelden
+  // zijn pas mogelijk nadat de observable klaar is
+  // Plaats bewerkingen in de tap() timeout() callback
   ngOnInit() {
     this.codeExamples$ = combineLatest([
       this.sharedService.currentChapter$,
@@ -34,14 +53,23 @@ export class CodeComponent implements OnInit {
         x;
         setTimeout(() => {
           this.highlightService.highlightAll();
+          this.observeCodeWrapper.observe(this.divCodeWrapper.nativeElement, this.config);
         }, 0)
       })
     );
   }
 
-
-
-
+  updateView(mutations: MutationRecord[]) {
+    mutations.forEach((mutation) => {
+      this.btnReset.nativeElement.style.opacity = '1';
+      if (mutation.target.parentElement?.classList.contains('example-html')) {
+        this.divExampleView.nativeElement.innerHTML = this.exampleHtml.nativeElement.textContent;
+      }
+      if (mutation.target.parentElement?.classList.contains('example-css')) {
+        this.exampleStyle.nativeElement.innerHTML = this.exampleCss.nativeElement.textContent;
+      }
+    })
+  }
 
   onPaste() {
     return
--- src/app/services/highlight.service.ts
+++ src/app/services/highlight.service.ts
@@ -7,6 +7,7 @@ import 'prismjs';
 import 'prismjs/components/prism-css';
 import 'prismjs/components/prism-javascript';
 import 'prismjs/components/prism-markup';
+import 'prismjs/plugins/normalize-whitespace/prism-normalize-whitespace';
 
 declare const Prism: any;
 
@@ -21,8 +22,17 @@ export class HighlightService {
 
   highlightAll() {
     if (isPlatformBrowser(this.platformId)) {
-      console.log('service')
       Prism.highlightAll()
+      Prism.plugins.NormalizeWhitespace.setDefaults({
+        'remove-trailing': true,
+        'remove-indent': true,
+        'left-trim': true,
+        'right-trim': true,
+        'indent': 0,
+        'remove-initial-line-feed': true,
+        'tabs-to-spaces': 2,
+      });
+
     }
   }
 }
