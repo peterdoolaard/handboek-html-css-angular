e48889a638eadbc603e0c6b41f6b4172033a6fb9
peterdoolaard <peter.doolaard@xs4all.nl>
2023-11-08 13:07:05 +0100

    Removing spec files

--- src/app/pipes/escape-markup.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { EscapeMarkupPipe } from './escape-markup.pipe';
-
-describe('EscapeMarkupPipe', () => {
-  it('create an instance', () => {
-    const pipe = new EscapeMarkupPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
