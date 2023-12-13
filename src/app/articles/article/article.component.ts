8d14d92d98e7807a21913845edb3cc1ab8da782c
peterdoolaard <peter.doolaard@xs4all.nl>
2023-12-13 14:38:55 +0100

    Working on the styling. Articles should look the same on every page

--- src/app/articles/article/article.component.ts
+++ src/app/articles/article/article.component.ts
@@ -1,17 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { AppSharedService } from '../../services/app-shared.service';
 import { SafeHtml } from '@angular/platform-browser';
+import { HighlightService } from '../../services/highlight.service';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
-  styleUrls: ['./article.component.scss']
+  styleUrls: ['./article.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ArticleComponent implements OnInit {
   articleHtml: SafeHtml = '';
 
   constructor(
     private sharedService: AppSharedService,
+    private highlightService: HighlightService
   ) {
   }
 
@@ -19,6 +22,10 @@ export class ArticleComponent implements OnInit {
     this.sharedService.getArticle('assets/artikelen/minder-code-en-meer-overzicht-dankzij-css-nesting.html')
       .subscribe((html) => {
         this.articleHtml = html;
+        setTimeout(() => {
+          this.highlightService.highlightAll();
+        }, 50);
+
       });
   }
 }
