8db2ec80d884415218594f010ee5c169cdaf0955
peterdoolaard <peter.doolaard@xs4all.nl>
2024-02-08 14:15:42 +0100

    Fix artikel Baseline, homepage toont nu nieuwste 3 artikelen

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AppSharedService } from '../services/app-shared.service';
+import { Observable, shareReplay, tap } from 'rxjs';
+import { Article } from '../models';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+  articleList$!: Observable<Article[]>;
 
   constructor(
-    private appService: AppSharedService,
+    private sharedService: AppSharedService,
   ) {
+  }
+
+  ngOnInit() {
+    this.articleList$ = this.sharedService.loadArticlesList().pipe(
+      shareReplay(1),
+      tap(result => result.sort((e1, e2) =>
+        e2.date.localeCompare(e1.date)))
+    );
   }
 }
